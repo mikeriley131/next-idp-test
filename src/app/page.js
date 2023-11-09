ac720cc49a961cb5eeb8e4f860c83d7eec31ca66
Mike Riley <mikeriley131@gmail.com>
2023-11-08 19:40:20 -0700

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { Button } from "pyrographic-react-component-library";
 
 export default function Home() {
   return (
@@ -10,12 +12,18 @@ export default function Home() {
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
+          <Button
+            label="Button"
+            onClick={() => console.log("Button clicked")}
+          />
+        </div>
+        <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +99,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
